65bab8b12cf022f1e26c56cfb0e11956f4c1e129
Chaunhat <dklt0405@gmail.com>
2020-11-09 13:18:57 +0700

    tab navigation
    tabnavigation

--- App.js
+++ App.js
@@ -1,105 +1,69 @@
 import React, { Component } from "react";
 import { View, Text, FlatList, ActivityIndicator } from "react-native";
 import { Avatar, List, ListItem, SearchBar } from "react-native-elements";
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default class FlatListDemo extends React.Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      loading: false,
-      data: [],
-      page: 1,
-      seed: 1,
-      error: null,
-      refreshing: false,
-    };
-  }
-  componentDidMount() {
-    this.makeRemoteRequest();
-  }
-  // fetch api
-  makeRemoteRequest = () => {
-    const { page, seed } = this.state;
-    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
-    this.setState({ loading: true });
-    fetch(url)
-      .then((res) => res.json())
-      .then((res) => {
-        console.log(res);
-        console.log("asdaghdajkgh");
 
-        this.setState({
-          data: page === 1 ? res.results : [...this.state.data, ...res.results],
-          error: res.error || null,
-          loading: false,
-          refreshing: false,
-        });
-      })
-      .catch((error) => {
-        this.setState({ error, loading: false });
-      });
-  };
-// seperator
-  zzzseperator =() => {
-    return (
-      <View
-        style={{
-          height: 1,
-          width: "86%",
-          backgroundColor: "#CED0CE",
-          marginLeft: "14%"
-        }}
-      />)
-  }
-  // searchbar
-  searchbar = () => {
-    return <SearchBar
-              placeholder="Don't type here"
-              round/>
-             
-  }
-// footer
-  chan =() => {
-    if (!this.state.loading) return null;
+import { NavigationContainer } from '@react-navigation/native';
 
-    return (
-      <View
-        style={{
-          paddingVertical: 20,
-          borderTopWidth: 30,
-          borderColor: "#CED0CE"
-        }}
-      >
-        <ActivityIndicator animating size="large" color="#8376f9"/>
-      </View>);
-  };
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FlatListDemo from "./home";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-  render() {
-    return (
-      <FlatList 
-        data={this.state.data}
-        keyExtractor={item => item.email}
-        renderItem={({ item }) => (
-          <ListItem>
-            <Avatar
-              source={{
-                uri: item.picture.thumbnail,
-              }}
-            />
-            <ListItem.Content>
-              <ListItem.Title>
-                {`${item.name.first} ${item.name.last}`}
-              </ListItem.Title>
-              <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
-            </ListItem.Content>
-          </ListItem>
-        )}
-        // declare of searchar , seperator and footer in flatlist tag
-        ItemSeparatorComponent={this.zzzseperator}
-        ListHeaderComponent={this.searchbar}
-        ListFooterComponent={this.chan}
-      />
-    );
-  }
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings text!</Text>
+    </View>
+  );
 }
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+     <Text>homescreen text</Text>
+    </View>
+  );
+}
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+      // decor cho tab
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'ios-list-box' : 'ios-list';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+      // co the bo neu can
+       >
+        <Tab.Screen name="Home" component={FlatListDemo} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Homie" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
---
+++ home.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
+import { Avatar, List, ListItem, SearchBar } from "react-native-elements";
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+
+
+
+export default class FlatListDemo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      data: [],
+      page: 1,
+      seed: 1,
+      error: null,
+      refreshing: false,
+    };
+  }
+  componentDidMount() {
+    this.makeRemoteRequest();
+  }
+  // fetch api
+  makeRemoteRequest = () => {
+    const { page, seed } = this.state;
+    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
+    this.setState({ loading: true });
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        // console.log(res);
+        // console.log("asdaghdajkgh");
+
+        this.setState({
+          data: page === 1 ? res.results : [...this.state.data, ...res.results],
+          error: res.error || null,
+          loading: false,
+          refreshing: false,
+        });
+      })
+      .catch((error) => {
+        this.setState({ error, loading: false });
+      });
+  };
+// seperator
+  zzzseperator =() => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "86%",
+          backgroundColor: "#CED0CE",
+          marginLeft: "14%"
+        }}
+      />)
+  }
+  // searchbar
+  searchbar = () => {
+    return <SearchBar
+              placeholder="Don't type here"
+              round/>
+             
+  }
+// footer
+  chan =() => {
+    if (!this.state.loading) return null;
+
+    return (
+      <View
+        style={{
+          paddingVertical: 20,
+          borderTopWidth: 30,
+          borderColor: "#CED0CE"
+        }}
+      >
+        <ActivityIndicator animating size="large" color="#8376f9"/>
+      </View>);
+  };
+
+  render() {
+    return (
+      <FlatList 
+        data={this.state.data}
+        keyExtractor={item => item.email}
+        renderItem={({ item }) => (
+          <ListItem>
+            <Avatar
+              source={{
+                uri: item.picture.thumbnail,
+              }}
+            />
+            <ListItem.Content>
+              <ListItem.Title>
+                {`${item.name.first} ${item.name.last}`}
+              </ListItem.Title>
+              <ListItem.Subtitle>{item.email}</ListItem.Subtitle>
+            </ListItem.Content>
+          </ListItem>
+        )}
+        // declare of searchar , seperator and footer in flatlist tag
+        ItemSeparatorComponent={this.zzzseperator}
+        ListHeaderComponent={this.searchbar}
+        ListFooterComponent={this.chan}
+      />
+    );
+  }
+}
